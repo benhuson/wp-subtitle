81ced17eee145db44cdb4f661b3ab9f5f137ccb9
Ben Huson <ben@thewhiteroom.net>
2014-03-11 21:19:55 +0000

    Methods should be declared as static.

--- wp-subtitle.php
+++ wp-subtitle.php
@@ -42,7 +42,7 @@ class WPSubtitle {
 	 * @uses  WPSubtitle::_add_meta_boxes()
 	 * @uses  WPSubtitle::_save_post()
 	 */
-	function _init() {
+	static function _init() {
 		add_action( 'init', array( 'WPSubtitle', '_add_default_post_type_support' ), 5 );
 		add_action( 'add_meta_boxes', array( 'WPSubtitle', '_add_meta_boxes' ) );
 		add_action( 'save_post', array( 'WPSubtitle', '_save_post' ) );
@@ -54,7 +54,7 @@ class WPSubtitle {
 	 * @since  2.0
 	 * @internal
 	 */
-	function _add_default_post_type_support() {
+	static function _add_default_post_type_support() {
 		add_post_type_support( 'page', 'wps_subtitle' );
 		add_post_type_support( 'post', 'wps_subtitle' );
 	}
@@ -69,7 +69,7 @@ class WPSubtitle {
 	 * @uses  apply_filters( 'wps_meta_box_title' )
 	 * @uses  WPSubtitle::_add_subtitle_meta_box()
 	 */
-	function _add_meta_boxes() {
+	static function _add_meta_boxes() {
 		$post_types = WPSubtitle::get_supported_post_types();
 		foreach ( $post_types as $post_type ) {
 			$meta_box_title = apply_filters( 'wps_meta_box_title', 'Subtitle', $post_type );
@@ -86,7 +86,7 @@ class WPSubtitle {
 	 * @uses  WPSubtitle::_get_post_meta()
 	 * @uses  apply_filters( 'wps_subtitle_field_description' )
 	 */
-	function _add_subtitle_meta_box() {
+	static function _add_subtitle_meta_box() {
 		global $post;
 		echo '<input type="hidden" name="wps_noncename" id="wps_noncename" value="' . wp_create_nonce( 'wp-subtitle' ) . '" />
 			<input type="text" id="wpsubtitle" name="wps_subtitle" value="' . WPSubtitle::_get_post_meta( $post->ID ) . '" style="width:99%;" />';
@@ -103,7 +103,7 @@ class WPSubtitle {
 	 *
 	 * @param  int  $post_id  Post ID or object.
 	 */
-	function _save_post( $post_id ) {
+	static function _save_post( $post_id ) {
 
 		// Verify if this is an auto save routine. 
 		// If it is our form has not been submitted, so we dont want to do anything
@@ -132,7 +132,7 @@ class WPSubtitle {
 	 * @param   int  $post_id  Post ID.
 	 * @return  bool
 	 */
-	function _verify_post_edit_capability( $post_id ) {
+	static function _verify_post_edit_capability( $post_id ) {
 		$abort = true;
 		$post_types = WPSubtitle::get_supported_post_types();
 		$post_types_obj = (array) get_post_types( array(
@@ -162,7 +162,7 @@ class WPSubtitle {
 	 * @param   string  $action  Nonce action.
 	 * @return  bool
 	 */
-	function _verify_posted_nonce( $nonce, $action ) {
+	static function _verify_posted_nonce( $nonce, $action ) {
 		if ( isset( $_POST[$nonce] ) && wp_verify_nonce( $_POST[$nonce], $action ) )
 			return true;
 		return false;
@@ -175,7 +175,7 @@ class WPSubtitle {
 	 *
 	 * @return  array  Array of supported post types.
 	 */
-	function get_supported_post_types() {
+	static function get_supported_post_types() {
 		$post_types = (array) get_post_types( array(
 			'_builtin' => false
 		) );
@@ -199,7 +199,7 @@ class WPSubtitle {
 	 * @param   int|object  $post  Post ID or object.
 	 * @return  string             The filtered subtitle meta value.
 	 */
-	function get_the_subtitle( $post = 0 ) {
+	static function get_the_subtitle( $post = 0 ) {
 		$post = get_post( $post );
 		$subtitle = WPSubtitle::_get_post_meta( $post );
 		return apply_filters( 'wps_subtitle', $subtitle, $post );
@@ -214,7 +214,7 @@ class WPSubtitle {
 	 * @param   int|object  $post  Post ID or object.
 	 * @return  string             The subtitle meta value.
 	 */
-	function _get_post_meta( $id = 0 ) {
+	static function _get_post_meta( $id = 0 ) {
 		$post = get_post( $id );
 		return get_post_meta( $post->ID, 'wps_subtitle', true );
 	}
