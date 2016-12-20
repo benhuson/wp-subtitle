4504dd5d1a3c43e396d20fa7339b8f22415d7827
Ben Huson <ben@thewhiteroom.net>
2016-12-20 22:03:54 +0000

    Add `wps_subtitle` support to REST API.

---
+++ includes/rest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @package     WP Subtitle
+ * @subpackage  REST API
+ */
+
+class WPSubtitle_REST {
+
+	/**
+	 * Constructor
+	 *
+	 * @internal  Do not create multiple instances.
+	 */
+	public function __construct() {
+
+		add_action( 'rest_api_init', array( $this, 'register_rest_field' ) );
+
+	}
+
+	/**
+	 * Register REST Field
+	 *
+	 * @internal  Called via the `rest_api_init` action.
+	 */
+	public function register_rest_field() {
+
+		$post_types = WPSubtitle::get_supported_post_types();
+
+		foreach ( $post_types as $post_type ) {
+
+			register_rest_field( $post_types, 'wps_subtitle', array(
+				'get_callback'    => array( $this, 'get_rest_field' ),
+				'update_callback' => null,
+				'schema'          => null
+			) );
+
+		}
+
+	}
+
+	/**
+	 * Get REST Field
+	 *
+	 * @internal  Called via register_rest_field() callback.
+	 *
+	 * @param   array            $object      Current post details.
+	 * @param   string           $field_name  Name of field.
+	 * @param   WP_REST_Request  $request     Current request.
+	 * @return  string                        Subtitle
+	 */
+	public function get_rest_field( $object, $field_name, $request ) {
+
+		$subtitle = new WP_Subtitle( $object['id'] );
+
+		return $subtitle->get_raw_subtitle();
+
+	}
+
+}
