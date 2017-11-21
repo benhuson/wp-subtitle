eb770e2199630f2f7a83b5d50a87f6ca3e09a68a
Ben Huson <ben@thewhiteroom.net>
2017-11-21 21:08:09 +0000

    Add WooCommerce Settings: WooCommerce > Settings > Products > Display

--- compat/woocommerce.php
+++ compat/woocommerce.php
@@ -20,6 +20,7 @@ class WPSubtitle_WooCommerce {
 		add_action( 'init', array( $this, 'add_product_post_type_support' ) );
 		add_action( 'woocommerce_single_product_summary' , array( $this, 'single_product_summary' ), 6 );
 		add_action( 'woocommerce_shop_loop_item_title' , array( $this, 'shop_loop_item_title' ) );
+		add_filter( 'woocommerce_product_settings' , array( $this, 'product_settings' ) );
 
 	}
 
@@ -53,6 +54,61 @@ class WPSubtitle_WooCommerce {
 	public function shop_loop_item_title() {
 
 		the_subtitle( '<p class="wp-subtitle">', '</p>' );
+
+	}
+
+	/**
+	 * Product Settings
+	 *
+	 * @param   array  $settings  Settings.
+	 * @return  array             Settings.
+	 *
+	 * @internal  Private. Called via the `woocommerce_product_settings` filter.
+	 */
+	public function product_settings( $settings ) {
+
+		$subtitle_settings = array(
+
+			array(
+				'title' => __( 'WP Subtitle', 'wp-subtitle' ),
+				'type'  => 'title',
+				'desc'  => '',
+				'id'    => 'wp_subtitle_options'
+			),
+
+			array(
+				'title'   => __( 'Enable Product Subtitles', 'wp-subtitle' ),
+				'desc'    => __( 'Add subtitle field to product edit screen', 'wp-subtitle' ),
+				'id'      => 'wp_subtitle_woocommerce_enabled',
+				'default' => 'no',
+				'type'    => 'checkbox',
+			),
+
+			array(
+				'title'         => __( 'Subtitle Display', 'wp-subtitle' ),
+				'desc'          => __( 'Show on single product pages', 'wp-subtitle' ),
+				'id'            => 'wp_subtitle_woocommerce_show_on_single',
+				'default'       => 'yes',
+				'type'          => 'checkbox',
+				'checkboxgroup' => 'start'
+			),
+
+			array(
+				'desc'          => __( 'Show on product archives', 'wp-subtitle' ),
+				'id'            => 'wp_subtitle_woocommerce_show_in_loop',
+				'default'       => 'yes',
+				'type'          => 'checkbox',
+				'checkboxgroup' => 'end'
+			),
+
+			array(
+				'type' => 'sectionend',
+				'id'   => 'wp_subtitle_options'
+			)
+
+		);
+
+		return array_merge( $settings, $subtitle_settings );
 
 	}
 
